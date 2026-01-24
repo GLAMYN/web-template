8dbe34653ee1dba2abed1f3f24b5ef9b9bf90b6d
bablu <bk147411@gmail.com>
2026-01-24 08:13:21 +0530

    added changes

--- server/constants/commissions.js
+++ server/constants/commissions.js
@@ -4,7 +4,7 @@
 exports.recurringCommission = {
   providerCommission: {
     percentage: 5,
-    minimum_amount: 5,
+    minimum_amount: 500, // $5.00 in cents (subunits)
   },
   customerCommission: {
     percentage: 0,
